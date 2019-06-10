f87f4d777bf4cbc1440516913dc9e71a6217ddb6
StiansbD <46784875+StiansbD@users.noreply.github.com>
2019-06-10 15:47:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+fenetre = pygame.display.set_mode((640, 480))
+
+fond = pygame.image.load("level/tileset.png").convert()
+fenetre.blit(fond, (0,0))
+
+pygame.display.flip()
+
+continuer = 1
+while continuer:
+    continuer = int(input())
